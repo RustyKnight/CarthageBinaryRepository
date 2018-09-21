91764463360d01666c3c13e90811c43a09e19179
Shane Whitehead <shane.whitehead@beamcommunications.com>
2018-09-21 14:03:42 +1000

    Initial commit

---
+++ src/java/org/kaizen/cbr/EnrtyPoint.java
@@ -0,0 +1,270 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.kaizen.cbr;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.security.AccessController;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.kaizen.cbr.RepositoryManager.Binary;
+import org.kaizen.cbr.RepositoryManager.Version;
+
+/**
+ *
+ * @author shanewhitehead
+ */
+public class EnrtyPoint extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        System.out.println("contextPath = " + request.getContextPath());
+        System.out.println("queryString = " + request.getQueryString());
+        System.out.println("requestURI = " + request.getRequestURI());
+        System.out.println("requestURL = " + request.getRequestURL());
+
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet EnrtyPoint</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet EnrtyPoint at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    protected void processLibraryList(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>List of awesome libraries</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>List of awesome libraries</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    protected void processVersionList(String library, HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        List<RepositoryManager.Version> versions = RepositoryManager.INSTANCE.getVersions(getServletContext(), library);
+        if (versions.size() == 0) {
+            response.sendError(404, "No releases avaliable for " + library);
+            return;
+        }
+
+        response.setContentType("text/html;charset=UTF-8");
+
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>List avaliable versions for library</title>");
+            out.println("</head>");
+            out.println("<body>");
+
+            out.println("<h1>Avaliable releases for [" + library + "]</h1>");
+            out.println("<ul>");
+            for (Version version : versions) {
+                out.println("<li>" + version.toString() + "</li>");
+            }
+            out.println("</ul>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+    
+    protected void processVersionBinariesList(String library, String version, HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        List<Binary> binaries = RepositoryManager.INSTANCE.getBinariesAvaliableByVersion(getServletContext(), library, version);
+        if (binaries.size() == 0) {
+            response.sendError(404, "No binaries avaliable for " + library + "/v-" + version);
+            return;
+        }
+
+        response.setContentType("text/html;charset=UTF-8");
+
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>List avaliable versions for library</title>");
+            out.println("</head>");
+            out.println("<body>");
+
+            out.println("<h1>Avaliable Xcode binary releases for [" + library + "/v" + version + "]</h1>");
+            out.println("<ul>");
+            for (Binary binary : binaries) {
+                String name = binary.getBinary().getName();
+                name = name.replace("Xcode-", "");
+                name = name.replace("Xcode", "");
+                name = name.replace("-framework.zip", "");
+                name = name.replace(".framework.zip", "");
+                name = name.replace("framework.zip", "");
+                out.println("<li>" + name + "</li>");
+            }
+            out.println("</ul>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    protected void processXcodeBinariesList(String library, String xcodeVersion, HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        List<Binary> binaries = RepositoryManager.INSTANCE.getXcodeBinaries(getServletContext(), library, xcodeVersion);
+        if (binaries.size() == 0) {
+            response.sendError(404, "No binaries avaliable for " + library + "/Xcode-" + xcodeVersion);
+            return;
+        }
+
+        response.setContentType("application/json");
+
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("{");
+            String path = "https://" + request.getServerName() + ":" + request.getServerPort() + "/";
+            for (Binary binary : binaries) {
+                // At this point, the name is irrelevent, it only matters that 
+                // it is carried as part of the overall query
+                out.println("\t\"" + binary.getVesion() + "\": \"" + path + library + "/" + binary.getVesion() + "/" + xcodeVersion + "-framework.zip\"");
+            }
+            out.println("}");
+        }
+    }
+
+    protected void processBinary(String library, String version, String xcodeVersion, HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        xcodeVersion = xcodeVersion.replace("-framework.zip", "");
+
+        File file = RepositoryManager.INSTANCE.getBinary(getServletContext(), library, version, xcodeVersion);
+        if (file == null) {
+            response.sendError(404, "No binary avaliable for " + library + "/" + version + "-" + xcodeVersion);
+        }
+
+        response.setContentType("application/zip");
+        response.setHeader("Content-disposition", "attachment; filename=" + library + "-v" + version + "-Xcode-" + xcodeVersion + "-framework.zip");
+        response.setContentLengthLong(file.length());
+
+        try (FileInputStream fis = new FileInputStream(file)) {
+            OutputStream out = response.getOutputStream();
+            byte[] buffer = new byte[4096];
+            int bytesRead = -1;
+            while ((bytesRead = fis.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+            }
+            out.flush();
+        }
+    }
+
+    protected void processBadRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.sendError(404, "Unknown resource");
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+//        processRequest(request, response);
+        String path = request.getRequestURI();
+        System.out.println("Path = " + path);
+        List<String> parts = new ArrayList<String>(Arrays.asList(path.split("\\/")));
+        if (parts.size() > 0) {
+            parts.remove(0);
+        }
+        System.out.println("Path elements = " + parts.size());
+        System.out.println("Path elements = " + parts);
+        if (parts.size() == 0) {
+            // Do we want to list the libaries?
+            System.out.println("List libraries");
+            processLibraryList(request, response);
+        } else if (parts.size() == 1) {
+            System.out.println("List library versions");
+            processVersionList(parts.get(0), request, response);
+        } else if (parts.size() == 2) {
+            String text = parts.get(1);
+            if (Version.isVersion(text)) {
+                System.out.println("List library, binaries avaliable for version");
+                processVersionBinariesList(parts.get(0), parts.get(1), request, response);
+            } else {
+                System.out.println("List library, binaries avaliable for xcode");
+                processXcodeBinariesList(parts.get(0), parts.get(1), request, response);
+            }
+        } else if (parts.size() == 3) {
+            System.out.println("Get binary");
+            processBinary(parts.get(0), parts.get(1), parts.get(2), request, response);
+        } else {
+            processBadRequest(request, response);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Carthage Binary Repository";
+    }// </editor-fold>
+
+}
---
+++ src/java/org/kaizen/cbr/RepositoryManager.java
@@ -0,0 +1,255 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.kaizen.cbr;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.IllegalFormatException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletContext;
+
+/**
+ *
+ * @author shanewhitehead
+ */
+public enum RepositoryManager {
+    INSTANCE;
+
+    protected static final String REPOSITORY_LOCATION_KEY = "Repository.location";
+
+    private RepositoryManager() {
+    }
+
+    protected String getRepositoryRoot(ServletContext context) {
+        return context.getInitParameter(REPOSITORY_LOCATION_KEY);
+    }
+
+    public List<Version> getVersions(ServletContext context, String library) {
+        ArrayList<Version> versions = new ArrayList<>();
+        String path = getRepositoryRoot(context);
+        if (path == null) {
+            System.out.println("No repository path configured");
+            return versions;
+        }
+        File filePath = new File(path, library);
+        if (!filePath.exists() || !filePath.isDirectory()) {
+            System.out.println(filePath + " either does not exist or is not a directory");
+            return versions;
+        }
+        File[] avaliableVersions = filePath.listFiles((File pathname) -> {
+            String name1 = pathname.getName();
+            return name1.matches("^[0-9]+.[0-9]+.[0-9]+$");
+        });
+
+        for (File version : avaliableVersions) {
+            try {
+                versions.add(new Version(version.getName()));
+            } catch (InvalidParameterException exp) {
+                System.out.println(version.getName() + " is an invalid directory");
+            }
+        }
+
+        Collections.sort(versions, new Comparator<Version>() {
+            @Override
+            public int compare(Version o1, Version o2) {
+                return o1.compareTo(o2);
+            }
+        });
+
+        return versions;
+    }
+
+    public List<Binary> getBinariesAvaliableByVersion(ServletContext context, String library, String version) {
+        String root = getRepositoryRoot(context);
+        if (root == null) {
+            System.out.println("No repository path configured");
+            return new ArrayList<>();
+        }
+
+        ArrayList<Binary> binaries = new ArrayList<>(25);
+        try {
+            Version libraryVersion = new Version(version);
+            File path = new File(new File(root, library), version);
+            File[] releases = path.listFiles(new FileFilter() {
+                @Override
+                public boolean accept(File pathname) {
+                    return pathname.getName().toLowerCase().endsWith(".zip");
+                }
+            });
+            for (File file : releases) {
+                binaries.add(new Binary(libraryVersion, file));                
+            }
+        } catch (InvalidParameterException ex) {
+            return new ArrayList<>();
+        }
+        return binaries;
+    }
+
+    public List<Binary> getXcodeBinaries(ServletContext context, String library, String xCodeVersion) {
+        String path = getRepositoryRoot(context);
+        if (path == null) {
+            System.out.println("No repository path configured");
+            return new ArrayList<>();
+        }
+        File filePath = new File(path, library);
+
+        return findReleases(filePath, xCodeVersion);
+    }
+
+    protected List<Binary> findReleases(File root, String xCodeVersion) {
+        List<Binary> binaries = new ArrayList<Binary>();
+        File[] files = root.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                try {
+                    Version version = new Version(file.getName());
+                    binaries.addAll(findBinaries(file, xCodeVersion, version));
+                } catch (InvalidParameterException ex) {
+                }
+            }
+        }
+        return binaries;
+    }
+
+    protected List<Binary> findBinaries(File root, String xCodeVersion, Version version) {
+        List<Binary> binaries = new ArrayList<Binary>();
+        File[] files = root.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                return pathname.getName().toLowerCase().endsWith(".zip");
+            }
+        });
+        // We'd be lucky ... or stupid ... to find more then one
+        for (File file : files) {
+            if (file.getName().contains(xCodeVersion)) {
+                binaries.add(new Binary(version, file));
+            }
+        }
+        return binaries;
+    }
+
+    public File getBinary(ServletContext context, String library, String version, String xcodeVersion) {
+        String path = getRepositoryRoot(context);
+        if (path == null) {
+            System.out.println("No repository path configured");
+            return null;
+        }
+        File filePath = new File(new File(path, library), version);
+        File[] files = filePath.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                String name = pathname.getName().toLowerCase();
+                return name.endsWith(".zip") && name.contains(xcodeVersion.toLowerCase());
+            }
+        });
+        if (files.length == 0) {
+            return null;
+        }
+
+        System.out.println("Requesting binary for " + library + "/" + version + "/" + xcodeVersion + " sending " + files[0].getPath());
+
+        return files[0];
+    }
+
+    public class Binary {
+
+        private Version vesion;
+        private File binary;
+
+        public Binary(Version vesion, File binary) {
+            this.vesion = vesion;
+            this.binary = binary;
+        }
+
+        public Version getVesion() {
+            return vesion;
+        }
+
+        public File getBinary() {
+            return binary;
+        }
+
+    }
+
+    public static class InvalidParameterException extends Exception {
+
+        public InvalidParameterException(String message) {
+            super(message);
+        }
+
+        public InvalidParameterException(String message, Exception cause) {
+            super(message, cause);
+        }
+    }
+
+    public static class Version implements Comparable<Version> {
+        
+        public static boolean isVersion(String text) {
+            try {
+                Version version = new Version(text);
+            } catch (InvalidParameterException ex) {
+                return false;
+            }
+            return true;
+        }
+
+        private int major;
+        private int minor;
+        private int patch;
+
+        public Version(String version) throws InvalidParameterException {
+            String[] parts = version.split("\\.");
+            if (parts.length != 3) {
+                throw new InvalidParameterException("Version value must be in {major}.{minor}.{patch} format");
+            }
+            try {
+                major = Integer.parseInt(parts[0]);
+                minor = Integer.parseInt(parts[1]);
+                patch = Integer.parseInt(parts[2]);
+            } catch (NumberFormatException exp) {
+                throw new InvalidParameterException("Version value must be in {major}.{minor}.{patch} format", exp);
+            }
+        }
+
+        public int getMajor() {
+            return major;
+        }
+
+        public int getMinor() {
+            return minor;
+        }
+
+        public int getPatch() {
+            return patch;
+        }
+
+        @Override
+        public String toString() {
+            return getMajor() + "." + getMinor() + "." + getPatch();
+        }
+
+        @Override
+        public int compareTo(Version o) {
+            if (getMajor() != o.getMajor()) {
+                return getMajor() - o.getMajor();
+            }
+            if (getMinor() != o.getMinor()) {
+                return getMinor() - o.getMinor();
+            }
+            if (getPatch() != o.getPatch()) {
+                return getPatch() - o.getPatch();
+            }
+            return 0;
+        }
+
+    }
+}
